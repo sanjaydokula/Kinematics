3eca3a70522361200c88849810f86ac9d311ca71
sanjaydokula <50962362+sanjaydokula@users.noreply.github.com>
2022-07-21 01:27:45 +0530

    init

---
+++ Assets/Scripts/Kinematics.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kinematics : MonoBehaviour
+{
+    // Start is called before the first frame update
+    List<Segment> segs = new List<Segment>();
+    Vector3[] points;
+    GameObject go;
+    [SerializeField]
+    float angleInc1 = 10.0f;
+    [SerializeField]
+    float angleInc2 = 10.0f;
+    [SerializeField]
+    float angleInc3 = 10.0f;
+    [SerializeField]
+    float angleInc4 = 10.0f;
+    float phase = Mathf.PI;
+    public Color c1 = Color.yellow;
+    public Color c2 = Color.red;
+    void Start()
+    {
+        Segment seg1 = new Segment(new Vector3(0f, 0f, 0f), 0.0f, 4.0f, "seg1");
+        Segment segp = seg1;
+        for(int i = 0; i < 40; i++)
+        {
+            Segment seg = new Segment(segp, 0, 2, "seg");
+            segs.Add(seg);
+            segp = seg;
+        }
+
+
+
+        //Segment seg2 = new Segment(seg1, 45.0f, 4.0f, "seg2");
+        //Segment seg3 = new Segment(seg2, 20.0f, 4.0f, "seg3");
+        //Segment seg4 = new Segment(seg3, 0.0f, 4.0f, "seg4");
+        ////Segment2 seg3 = new Segment2(seg2,new Vector3(8f,0f,0f),0.0f,"seg3");
+        //segs.Add(seg1);
+        //segs.Add(seg2);
+        //segs.Add(seg3);
+        //segs.Add(seg4);
+
+
+
+        go = new GameObject();
+        go.transform.SetParent(this.gameObject.transform);
+        go.AddComponent<LineRenderer>();
+        go.GetComponent<LineRenderer>().material = new Material(Shader.Find("Sprites/Default"));
+        go.GetComponent<LineRenderer>().widthMultiplier = 0.2f;
+        float alpha = 1.0f;
+        Gradient gradient = new Gradient();
+        gradient.SetKeys(
+            new GradientColorKey[] { new GradientColorKey(c1, 0.0f), new GradientColorKey(c2, 1.0f) },
+            new GradientAlphaKey[] { new GradientAlphaKey(alpha, 0.0f), new GradientAlphaKey(alpha, 1.0f) }
+        );
+        go.GetComponent<LineRenderer>().colorGradient = gradient;
+        //go.GetComponent<LineRenderer>().col
+        go.GetComponent<LineRenderer>().positionCount = (segs.Count) + 1;
+        //setPoints(points, segs);
+        go.GetComponent<LineRenderer>().SetPositions(setPoints(segs));
+        //Debug.Log("calling updatePoints-main");
+        //Debug.Log("exited updatePoints-main");
+        //segs[1].rotate(0.0f);
+        //updatePoints();
+        //go.GetComponent<LineRenderer>().SetPositions(setPoints(segs));
+    }
+
+    Vector3[] setPoints(List<Segment> segs)
+    {
+        points = new Vector3[segs.Count + 1];
+
+        for (int i = 0; i < segs.Count; i++)
+        {
+
+            points[i] = segs[i].A;
+            points[i + 1] = segs[i].B;
+        }
+        return points;
+    }
+    void updatePoints()
+    {
+        //updating segment points.
+        foreach (var item in segs)
+        {
+            //Debug.Log("calling updatePoints-main for seg. " + item.name);
+            item.calc_updatePoints();
+        }
+        //Debug.Log("exiting updatePoints-main");
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Debug.Log("update");
+        for (int i = 0; i < segs.Count; i++)
+        {
+            segs[i].rotate(Mathf.Sin(i+phase*Time.time) * angleInc1);
+        }
+        updatePoints();
+        go.GetComponent<LineRenderer>().SetPositions(setPoints(segs));
+
+
+    }
+}
---
+++ Assets/Scripts/Segment.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Segment
+{
+    // Start is called before the first frame update
+    Vector3 a;
+    float angle;
+    float local_angle;
+    Vector3 b;
+    float seg_length;
+    public string name;
+    Segment parent_seg = null;
+    public Segment(Vector3 a, float angle, float seg_length, string name)
+    {
+        this.a = a;
+        this.angle = angle;
+        this.local_angle = this.angle;
+        this.seg_length = seg_length;
+        this.name = name;
+        this.b = calculateB();
+    }
+
+    public Segment(Segment seg, float angle, float seg_length, string name)
+    {
+        this.a = seg.B;
+        this.seg_length = seg_length;
+        parent_seg = seg;
+        this.angle = angle;
+        this.local_angle = this.angle;
+        this.name = name;
+        this.b = calculateB();
+        //Debug.Log(this.name + "parent_seg" + parent_seg);
+    }
+
+    public Vector3 A { get => a; set => a = value; }
+    public Vector3 B { get => b; set => b = value; }
+    public float Angle { get => angle; set => angle = value; }
+
+    public Vector3 calculateB()
+    {
+        float r = this.seg_length;
+        Vector3 b = new Vector3(0f, 0f, 0f);
+        float dx = r * Mathf.Cos((this.angle * Mathf.PI) / 180);
+        float dy = r * Mathf.Sin((this.angle * Mathf.PI) / 180);
+        b.x = this.a.x + dx;
+        b.y = this.a.y + dy;
+        return b;
+
+    }
+    public void calc_updatePoints()
+    {
+        this.angle = local_angle;
+        if (this.parent_seg != null)
+        {
+
+            this.a = this.parent_seg.B;
+            this.angle += this.parent_seg.angle;
+
+        }
+        //Debug.Log(this.a);
+        this.b = calculateB();
+    }
+
+    public void rotate(float angle)
+    {
+        this.local_angle += angle;
+    }
+}
